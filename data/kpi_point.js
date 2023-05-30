929cfd96c3202a6f3570bb2e2c87404dff096d5d
nafranandya <nafranandya@mail.ugm.ac.id>
2023-05-30 12:53:50 +0700

    first-commit

---
+++ data/kpi_point.js
@@ -0,0 +1,28 @@
+var kpi_point = {
+    "type": "FeatureCollection",
+    "name": "kpi_point",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "FID_": 1, "admin": "Kec. Loa Kulu", "Status_KPI": "Kawasan Industri", "X": 503049.8387, "Y": 9941864.896 }, "geometry": { "type": "Point", "coordinates": [ 117.032911195465317, -0.517969224294768 ] } },
+    { "type": "Feature", "properties": { "FID_": 8, "admin": "Kec. Samboja", "Status_KPI": "Kawasan Industri", "X": 505048.1231, "Y": 9899325.084000001 }, "geometry": { "type": "Point", "coordinates": [ 117.045371897727804, -0.910836488471501 ] } },
+    { "type": "Feature", "properties": { "FID_": 9, "admin": "Kec. Muara Jawa", "Status_KPI": "Kawasan Industri", "X": 502019.8241, "Y": 9910780.944 }, "geometry": { "type": "Point", "coordinates": [ 117.018153438382541, -0.807192210280758 ] } },
+    { "type": "Feature", "properties": { "FID_": 12, "admin": "Kec. Anggana", "Status_KPI": "Kawasan Industri", "X": 529894.1627, "Y": 9936948.714 }, "geometry": { "type": "Point", "coordinates": [ 117.268663528237227, -0.570438261009257 ] } },
+    { "type": "Feature", "properties": { "FID_": 14, "admin": "Kec. Loa Janan", "Status_KPI": "Kawasan Industri", "X": 505551.6416, "Y": 9934818.955 }, "geometry": { "type": "Point", "coordinates": [ 117.049893820163021, -0.58971292696528 ] } },
+    { "type": "Feature", "properties": { "FID_": 15, "admin": "Kec. Loa Kulu", "Status_KPI": "Kawasan Industri", "X": 505733.2102, "Y": 9941759.712 }, "geometry": null },
+    { "type": "Feature", "properties": { "FID_": 16, "admin": "Kec. Loa Kulu", "Status_KPI": "Kawasan Industri", "X": 503417.634, "Y": 9945144.255000001 }, "geometry": null },
+    { "type": "Feature", "properties": { "FID_": 17, "admin": "Kec. Loa Kulu", "Status_KPI": "Kawasan Industri", "X": 501817.9427, "Y": 9946580.122 }, "geometry": null },
+    { "type": "Feature", "properties": { "FID_": 18, "admin": "Kec. Tenggarong", "Status_KPI": "Kawasan Industri", "X": 498668.8639, "Y": 9949511.497 }, "geometry": { "type": "Point", "coordinates": [ 116.988037038383595, -0.456785217480665 ] } },
+    { "type": "Feature", "properties": { "FID_": 23, "admin": "Kec. Sebulu", "Status_KPI": "Kawasan Industri", "X": 474967.7211, "Y": 9953137.51 }, "geometry": { "type": "Point", "coordinates": [ 116.775035693107924, -0.423976279885137 ] } },
+    { "type": "Feature", "properties": { "FID_": 30, "admin": "Kec. Kota Bangun", "Status_KPI": "Kawasan Industri", "X": 458593.2814, "Y": 9972940.964 }, "geometry": { "type": "Point", "coordinates": [ 116.62788753308449, -0.244806374292844 ] } },
+    { "type": "Feature", "properties": { "FID_": 32, "admin": "Kec. Muara Kaman", "Status_KPI": "Kawasan Industri", "X": 473448.3627, "Y": 9983636.013 }, "geometry": { "type": "Point", "coordinates": [ 116.761387041940125, -0.148048827880753 ] } },
+    { "type": "Feature", "properties": { "FID_": 33, "admin": "Kec. Muara Kaman", "Status_KPI": "Kawasan Industri", "X": 482391.5221, "Y": 9984994.262 }, "geometry": { "type": "Point", "coordinates": [ 116.841756815827424, -0.135761095099319 ] } },
+    { "type": "Feature", "properties": { "FID_": 36, "admin": "Kec. Marangkayu", "Status_KPI": "Kawasan Industri", "X": 542209.7159, "Y": 9992552.796 }, "geometry": { "type": "Point", "coordinates": [ 117.37932553133156, -0.067375703892953 ] } },
+    { "type": "Feature", "properties": { "FID_": 37, "admin": "Kec. Kenohan", "Status_KPI": "Kawasan Industri", "X": 429386.0568, "Y": 10009029.36 }, "geometry": { "type": "Point", "coordinates": [ 116.365422740305092, 0.081686407751562 ] } },
+    { "type": "Feature", "properties": { "FID_": 39, "admin": "Kec. Kembang Janggut", "Status_KPI": "Kawasan Industri", "X": 425905.0228, "Y": 10019405.39 }, "geometry": { "type": "Point", "coordinates": [ 116.334139130764271, 0.175554709631134 ] } },
+    { "type": "Feature", "properties": { "FID_": 42, "admin": "Kec. Tabang", "Status_KPI": "Kawasan Industri", "X": 383266.6806, "Y": 10046487.2 }, "geometry": { "type": "Point", "coordinates": [ 115.957669109917106, 0.421571234082269 ] } },
+    { "type": "Feature", "properties": { "FID_": 43, "admin": "Kec. Tabang", "Status_KPI": "Kawasan Industri", "X": 366559.1516, "Y": 10046101.24 }, "geometry": { "type": "Point", "coordinates": [ 115.806951533029519, 0.417455265950838 ] } },
+    { "type": "Feature", "properties": { "FID_": 45, "admin": "Kec. Kota Bangun", "Status_KPI": "Kawasan Industri", "X": 451916.3074, "Y": 9967706.209000001 }, "geometry": { "type": "Point", "coordinates": [ 116.573291470702202, -0.29366504586947 ] } },
+    { "type": "Feature", "properties": { "FID_": 47, "admin": "Kec. Muara Badak", "Status_KPI": "Kawasan Industri", "X": 545229.2838, "Y": 9963972.119000001 }, "geometry": { "type": "Point", "coordinates": [ 117.413658665147778, -0.323534771729015 ] } }
+    ]
+    }
+    
